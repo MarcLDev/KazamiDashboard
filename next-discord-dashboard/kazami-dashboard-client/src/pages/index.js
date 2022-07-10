bc5d9b05c9c22af25043e004a4814e3442896be5
MarcL <70085700+MarcLDev@users.noreply.github.com>
2022-07-10 19:40:01 -0300

    Front-end setting up

--- next-discord-dashboard/kazami-dashboard-client/src/pages/index.js
+++ next-discord-dashboard/kazami-dashboard-client/src/pages/index.js
@@ -1,3 +1,27 @@
+import styles from '../utils/styles/home.module.scss';
+import { FaDiscord } from 'react-icons/fa';
+import logo from '../static/images/kazami.png';
+
 export default function Home() {
-  return <div className="page">Home Page</div>
+
+  const handleLogin = () => {
+    window.location.href="http://localhost:3001/api/auth/discord";
+  };
+  return <div className="page aligned-center">
+
+    <div className='container'>
+      <a href='#' className='logo'>
+        <img src={logo}></img>
+      </a>  
+    </div>
+
+
+
+    <div>
+      <button className={styles.button} onClick={handleLogin}>
+        <FaDiscord size={35} color="#7289DA" />
+        <span>Login with Discord</span>
+      </button>
+    </div>
+  </div>
 }
